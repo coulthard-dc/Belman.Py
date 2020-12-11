b15a430c3fc0d58bbaf52ed18eeec4cc06c7fc9d
coulthard_dc <coulthard.dc1998@gmail.com>
2020-12-12 01:29:54 +0300

    writing a solution to a file

--- __main__.py
+++ __main__.py
@@ -10,10 +10,14 @@ def main():
     table = creating_table(A,0)
     B = np.array(calculation(A))
     table_result = creating_table(B,1)
-    print(table)
+    answer_file=open('answer.txt','w')
+    answer_file.write('Исходные данные: \n')
+    answer_file.write(str(table)+'\n')
+    answer_file.write('III: \n a) \n')
     for i in range(B.shape[0]):
-        print(B[i][0])
-        print(table_result[i])
+        answer_file.write(str(i+1)+') шаг: \n')
+        answer_file.write(str(B[i][0])+'\n')
+        answer_file.write(str(table_result[i])+'\n')
 
 
 if __name__ == '__main__':
