6c852330e8d2b59075be6335bbf32f924da62439
coulthard_dc <coulthard.dc1998@gmail.com>
2020-12-13 00:18:28 +0300

    fix

--- modules/creating_table.py
+++ modules/creating_table.py
@@ -13,20 +13,22 @@ def creating_table(A,flag):
             table.add_row(A[i]) # Построчное формирование таблицы из значений матрицы A
     else:
         table=[]; #Формирорование выхожного списка таблиц
-        for item in A: #Перебираем элементы входной матрцы
-            col=['U\\S'] #Создание заголовков таблицы
-            J=item[2]
-            B=item[1] #Вытаскиваем итоговую матрцу
+        #for item in A: #Перебираем элементы входной матрцы
+        for i in range(A.shape[0]):
+            col=['S'+str(A.shape[0]-i-1)+'\\U'+str(A.shape[0]-i)] #Создание заголовков таблицы
+            J=A[i][2]
+            B=A[i][1] #Вытаскиваем итоговую матрцу
             B=[list(row) for row in B]
-            for i in range(len(B)):
-                print (J[i])
-                B[i].append(str(J[i]))
+            for j in range(len(B)):
+                print (J[j])
+                B[j].append(str(J[j]))
             for row in B: # Перебор каждой строки итоговой матрицы
                 col.append(str(row[0]))
-            col.append('J(S)')
-            col.append('Uo')
+            col.append('J'+str(A.shape[0]-i)+'(S'+str(A.shape[0]-i-1)+')')
+            col.append('U'+str(A.shape[0]-i)+'опт')
             table_i=pt.PrettyTable()
             table_i.field_names=col
+            col=[]
             for row in B:
                 table_i.add_row(row) #Создание итоговой таблицы
             table.append(table_i) #Добавление итоговой таблицы в список таблиц
