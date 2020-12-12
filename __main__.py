fbd192e74a2f76c5adcd849eccb4b5b1f8a5c4aa
coulthard_dc <coulthard.dc1998@gmail.com>
2020-12-12 14:27:12 +0300

    fix

--- __main__.py
+++ __main__.py
@@ -6,9 +6,12 @@ from modules.creating_table import creating_table
 
 
 def main():
-    A = input_data()
+    #A = input_data()
+    A=[[0,0,0,0,0],[20,7,6,14,14],[40,23,23,21,20],[60,31,30,34,35]]
+    A=np.array(A)
     table = creating_table(A,0)
     B = np.array(calculation(A))
+    #print (B)
     table_result = creating_table(B,1)
     answer_file=open('answer.txt','w')
     answer_file.write('Исходные данные: \n')
--- modules/calculation.py
+++ modules/calculation.py
@@ -3,14 +3,18 @@ import numpy as np
 
 def calculation (A):
     result=[] #Формирование выходного списка: [[матрица B iго шага, str], ...]
-    B=np.zeros((A.shape[0],A.shape[0]+3)) #Формирование второй матрицы на основе первой
+    B=np.zeros((A.shape[0],A.shape[0]+2)) #Формирование второй матрицы на основе первой
+    tmp = np.zeros((A.shape[0],1))
     for i in range(B.shape[0]): #Заполнение первого столбца выходной матрциы
-        B[i][0]=A[i][0]
+        tmp[i][0] = A[i][0] #Формирование временного столбца
     for i in reversed(range(A.shape[1]-1)): #Решение начинается с конца; кол-во шагов равно кол-ву столбоцов F в матрице A
         if i==(A.shape[1]-2): #первая итерация отличается отпоследующих
             string = 'J'+str(i+1)+'(S'+str(i)+') = max(F'+str(i+1)+'(U'+str(i+1)+'))    0<=U'+str(i+1)+'<=S'+str(i) #формирование строки
             for j in range(A.shape[0]):
-                B[j][j+1]=A[j][i+1] #Заполнение диагонали выходной матрицы
+                B[j][j]=A[j][i+1] #Заполнение диагонали выходной матрицы
+                B[j][-2]=np.amax(B[j], axis=0) #Заполнение столбца J(S)
+                B[j][-1]=tmp[np.argmax(B[j])] #Заполнение столбца Uопт
+            B=np.hstack((tmp,B))
             result.append([string,B])
         else:
             sting = 'J' +str(i+1)+'(S'+str(i)+') = max(F'+str(i+1)+'(U'+str(i+1)+'+J'+str(i+2)+'(S'+str(i-1)+'-U'+str(i)+'))    0<=U'+str(i+1)+'<=S'+str(i)
